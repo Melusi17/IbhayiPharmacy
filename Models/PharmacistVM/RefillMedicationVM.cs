8cdba87745aeaaf1e1ffaefe13907d63168eaa85
Melusi Mvikeli Mamba <s224113038@mandela.ac.za>
2025-10-19 00:02:15 +0200

    request refill v1

---
+++ Models/PharmacistVM/RefillMedicationVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IbhayiPharmacy.Models.PharmacistVM
+{
+    public class RefillMedicationVM
+    {
+        public int ScriptLineID { get; set; }
+        public int PrescriptionID { get; set; }
+        public int MedicationID { get; set; }
+        public string MedicationName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public string Instructions { get; set; } = string.Empty;
+        public int TotalRepeats { get; set; }
+        public int RepeatsLeft { get; set; }
+        public string DoctorName { get; set; } = string.Empty;
+        public DateTime LastRefillDate { get; set; }
+        public int CurrentPrice { get; set; }
+        public string Schedule { get; set; } = string.Empty;
+    }
+}
